6ec8dc9288223f99eaed71256f69a87741976a5a
masashiGMS <masashi@gmsnet.com>
2013-11-13 14:07:57 -0800

    (iOS)add Console plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,3 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = []
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    }
+]
 });
