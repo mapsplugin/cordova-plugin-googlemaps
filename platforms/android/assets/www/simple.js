133871cb61ad0c3cfe67bf36b80144996c894b2b
Masashi Katsumata <wf9a5m75@gmail.com>
2014-04-06 13:38:37 +0900

    Temporally save

--- platforms/android/assets/www/simple.js
+++ platforms/android/assets/www/simple.js
@@ -94,6 +94,11 @@ function onMapReady() {
   });
   
   map.showDialog();
+  
+  return;
+  document.addEventListener("backbutton", function() {
+      alert("backbutton");
+  }, false);
   map.addKmlOverlay({
     'url': 'www/radio-folder.kml'
   //  'url': 'https://www.google.com/fusiontables/exporttable?query=select+col2+from+1-v6i33Lf_FjhRZcHKO0PG2DADipCg4L-dGiucAE&o=kml&g=col2'
