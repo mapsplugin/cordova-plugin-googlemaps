c224851809d1bdfcad3b047739e0dd929764153e
wf9a5m75 <wf9a5m75@gmail.com>
2014-05-13 09:05:44 -0700

    Bug fix #42: Fix typo

--- www/googlemaps-cdv-plugin.js
+++ www/googlemaps-cdv-plugin.js
@@ -1448,7 +1448,7 @@
     MY_LOCATION_CHANGE: 'my_location_change', // for Android
     MY_LOCATION_BUTTON_CLICK: 'my_location_button_click',
     CAMERA_CHANGE: 'camera_change',
-    CANERA_IDLE: 'camera_idle',
+    CAMERA_IDLE: 'camera_idle',
     MAP_READY: 'map_ready',
     MAP_LOADED: 'map_loaded', //for Android
     MAP_WILL_MOVE: 'will_move', //for iOS
