e0834d805c363a36abe5283bd98057300c089746
wf9a5m75 <wf9a5m75@gmail.com>
2014-08-08 22:23:02 -0700

    testing

--- touchTest/platforms/ios/touchTest/Plugins/TouchDetectingViewController.m
+++ touchTest/platforms/ios/touchTest/Plugins/TouchDetectingViewController.m
@@ -12,39 +12,4 @@
 @end
 
 @implementation TouchDetectingViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.mWindow = (TapDetectingWindow *)[[UIApplication sharedApplication].windows objectAtIndex:0];
-    self.mWindow.viewToObserve = self.mHtmlViewer;
-    self.mWindow.controllerThatObserves = self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
