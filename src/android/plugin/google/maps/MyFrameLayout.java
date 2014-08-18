724a4d97227773e52ab341debe564a384873fcb3
wf9a5m75 <wf9a5m75@gmail.com>
2014-08-18 16:45:48 -0700

    temporally save

--- src/android/plugin/google/maps/MyFrameLayout.java
+++ src/android/plugin/google/maps/MyFrameLayout.java
@@ -1,17 +1,22 @@
 package plugin.google.maps;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
 public class MyFrameLayout extends FrameLayout {
-
+  public Rect mapRect = new Rect();
+  
   public MyFrameLayout(Context context) {
     super(context);
   }
   
   @Override
   public boolean onInterceptTouchEvent(MotionEvent event) {
-    return true;
+    int x = (int)event.getX();
+    int y = (int)event.getY();
+    return mapRect.contains(x, y);
   }
 }
