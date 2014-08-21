906a99d520b9a2725df3307240eb015781d0434c
wf9a5m75 <wf9a5m75@gmail.com>
2014-08-20 19:29:38 -0700

    temporally save

--- src/android/plugin/google/maps/MyScrollView.java
+++ src/android/plugin/google/maps/MyScrollView.java
@@ -7,52 +7,9 @@ import android.widget.ScrollView;
 
 public class MyScrollView extends ScrollView {
   
-  public interface ScrollViewListener {
-    void onScrollChanged(MyScrollView scrollView, int x, int y, int oldx, int oldy);
-    void onEndScroll();
-  }
-  
-  private Runnable scrollerTask;
-  private int initialPosition;
-  
-  private ScrollViewListener scrollViewListener = null;
-  
   public MyScrollView(Context context) {
     super(context);
     
-    scrollerTask = new Runnable() {
-      public void run() {
-          if (initialPosition - getScrollY() == 0) {//has stopped
-            if (scrollViewListener != null) {
-              scrollViewListener.onEndScroll();
-            }
-          } else {
-            initialPosition = getScrollY();
-            MyScrollView.this.postDelayed(scrollerTask, 100);
-          }
-      }
-    };
-  }
-  @Override
-  public boolean onTouchEvent(MotionEvent ev) {
-    return true;
-  }
-  
-  public void setOnScrollViewListener(ScrollViewListener scrollViewListener) {
-    this.scrollViewListener = scrollViewListener;
-  }
-  
-  @Override
-  protected void onScrollChanged(int x, int y, int oldX, int oldY) {
-    super.onScrollChanged(x, y, oldX, oldY);
-    if (scrollViewListener != null) {
-      scrollViewListener.onScrollChanged(this, x, y, oldX, oldY);
-    }
-  }
-
-  public void startScrollerTask() {
-    initialPosition = getScrollY();
-    MyScrollView.this.postDelayed(scrollerTask, 100);
   }
 
 }
