ad0e1bae8c5822d93258e4b4244a7ea7bb6e21ad
Masashi Katsumata <wf9a5m75@gmail.com>
2014-10-29 11:22:48 -0700

    Update GoogleMaps.m

--- src/ios/GoogleMaps/GoogleMaps.m
+++ src/ios/GoogleMaps/GoogleMaps.m
@@ -59,7 +59,7 @@
  */
 -(void)versionCheck
 {
-  NSString *PLUGIN_VERSION = @"1.2.2";
+  NSString *PLUGIN_VERSION = @"1.2.3";
   NSLog(@"This app uses phonegap-googlemaps-plugin version %@", PLUGIN_VERSION);
   
   if ([PluginUtil isInDebugMode] == NO || [PluginUtil isIOS7_OR_OVER] == NO) {
