2a1a64ee110bf61ac0e19bc7d8ef373daeb2e20d
wf9a5m75 <wf9a5m75@gmail.com>
2015-04-07 00:20:29 -0700

    .gitignore is now working

--- plugins/net.yoik.cordova.plugins.screenorientation/www/screenorientation.android.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec'),
-    screenOrientation = {};
-
-screenOrientation.setOrientation = function(orientation) {
-    exec(null, null, "YoikScreenOrientation", "screenOrientation", ['set', orientation]);
-};
-
-module.exports = screenOrientation;
--- test_crosswalk_proj/CordovaLib/xwalk_core_library/src/README.md
+++
@@ -1,5 +0,0 @@
-# Source folder for xwalk library
-## Why it's empty
-xwalk library doesn't contain java sources.
-## Why put me here
-To make archives keep the folder, the src directory is needed to build an apk by ant.
