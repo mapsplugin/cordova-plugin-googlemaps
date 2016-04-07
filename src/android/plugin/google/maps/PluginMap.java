85f17e27d01d80a4ece82a795814b97310a05a89
masashiGMS <masashi@gmsnet.com>
2016-04-07 10:28:12 -0700

    Fix: typo

--- src/android/plugin/google/maps/PluginMap.java
+++ src/android/plugin/google/maps/PluginMap.java
@@ -416,7 +416,7 @@ public class PluginMap extends MyPlugin {
         : mapTypeId;
 
     if (mapTypeId == -1) {
-      callbackContext.error("Unknow MapTypeID is specified:" + typeStr);
+      callbackContext.error("Unknown MapTypeID is specified:" + typeStr);
       return;
     }
     
