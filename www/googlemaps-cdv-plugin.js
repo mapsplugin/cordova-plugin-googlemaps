33b363d79321e1eee5eb8e57900668289393a9f4
wf9a5m75 <wf9a5m75@gmail.com>
2017-07-11 16:36:48 -0700

    Bug fix: can not touch the map if go back from another view using the back button of Android

--- www/googlemaps-cdv-plugin.js
+++ www/googlemaps-cdv-plugin.js
@@ -287,6 +287,7 @@ var saltHash = Math.floor(Math.random() * Date.now());
     cacheZIndex = {};
     setTimeout(putHtmlElements, 0);
   }
+  document.addEventListener("backbutton", resetTimer);
   document.addEventListener("deviceready", resetTimer);
   document.addEventListener("plugin_touch", resetTimer);
   window.addEventListener("orientationchange", resetTimer);
