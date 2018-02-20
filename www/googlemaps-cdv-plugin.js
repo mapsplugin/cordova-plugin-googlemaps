dc23287f3b390bd29a1c40616070048af634f8ff
wf9a5m75 <wf9a5m75@gmail.com>
2018-02-20 15:18:31 -0800

    Code separating (#2086)

--- www/googlemaps-cdv-plugin.js
+++ www/googlemaps-cdv-plugin.js
@@ -1,44 +1,10 @@
 /* global cordova, plugin, CSSPrimitiveValue */
 var cordova_exec = require('cordova/exec');
 var execCmd = require("./commandQueueExecutor");
+var pluginInit = require("./pluginInit");
 
 var isSuspended = false;
-if (typeof Array.prototype.forEach !== "function") {
-  (function() {
-    Array.prototype.forEach = function(fn, thisArg) {
-      thisArg = thisArg || this;
-      for (var i = 0; i < this.length; i++) {
-        fn.call(thisArg, this[i], i, this);
-      }
-    };
-  })();
-}
-if (typeof Array.prototype.filter !== "function") {
-  (function() {
-    Array.prototype.filter = function(fn, thisArg) {
-      thisArg = thisArg || this;
-      var results = [];
-      for (var i = 0; i < this.length; i++) {
-        if (fn.call(thisArg, this[i], i, this) === true) {
-          results.push(this[i]);
-        }
-      }
-      return results;
-    };
-  })();
-}
-if (typeof Array.prototype.map !== "function") {
-  (function() {
-    Array.prototype.map = function(fn, thisArg) {
-      thisArg = thisArg || this;
-      var results = [];
-      for (var i = 0; i < this.length; i++) {
-        results.push(fn.call(thisArg, this[i], i, this));
-      }
-      return results;
-    };
-  })();
-}
+
 
 if (!cordova) {
   document.addEventListener("deviceready", function() {
@@ -81,81 +47,20 @@ if (!cordova) {
   var MAPS = {};
   var saltHash = Math.floor(Math.random() * Date.now());
 
-  /*****************************************************************************
-   * To prevent strange things happen,
-   * disable the changing of viewport zoom level by double clicking.
-   * This code has to run before the device ready event.
-   *****************************************************************************/
-  (function() {
-    var viewportTag = null;
-    var metaTags = document.getElementsByTagName('meta');
-    for (var i = 0; i < metaTags.length; i++) {
-        if (metaTags[i].getAttribute('name') === "viewport") {
-            viewportTag = metaTags[i];
-            break;
-        }
-    }
-    if (!viewportTag) {
-        viewportTag = document.createElement("meta");
-        viewportTag.setAttribute('name', 'viewport');
-    }
-
-    var viewportTagContent = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no';
-
-    // Detect if iOS device
-    if (/(iPhone|iPod|iPad)/i.test(window.navigator.userAgent)) {
-      // Get iOS major version
-      var iosVersion = parseInt((window.navigator.userAgent).match(/OS (\d+)_(\d+)_?(\d+)? like Mac OS X/i)[1]);
-      // Detect if device is running >iOS 11
-      // iOS 11's UIWebView and WKWebView changes the viewport behaviour to render viewport without the status bar. Need to override with "viewport-fit: cover" to include the status bar.
-      if (iosVersion >= 11) {
-        viewportTagContent += ', viewport-fit=cover';
-      }
-    }
-
-    // Update viewport tag attribute
-    viewportTag.setAttribute('content', viewportTagContent);
-  })();
-
-  /*****************************************************************************
-   * Prevent background, background-color, background-image properties
-   *****************************************************************************/
-  var navDecorBlocker = document.createElement("style");
-  navDecorBlocker.setAttribute("type", "text/css");
-  navDecorBlocker.innerText = [
-    "html, body, ._gmaps_cdv_ {",
-    "   background-image: url() !important;",
-    "   background: rgba(0,0,0,0) url() !important;",
-    "   background-color: rgba(0,0,0,0) !important;",
-    "}",
-    "._gmaps_cdv_ .nav-decor {",
-    "   background-color: rgba(0,0,0,0) !important;",
-    "   background: rgba(0,0,0,0) !important;",
-    "   display:none !important;",
-    "}"
-  ].join("");
-  document.head.appendChild(navDecorBlocker);
+  // The pluginInit.js must execute before loading HTML is completed.
+  pluginInit();
 
   /*****************************************************************************
    * Add event lister to all html nodes under the <body> tag.
    *****************************************************************************/
   (function() {
+    // If <body> is not ready yet, wait 25ms, then execute this function again.
     if (!document.body || !document.body.firstChild) {
       common.nextTick(arguments.callee, 25);
       return;
     }
 
-    //setTimeout(function() {
-      // Webkit redraw mandatory
-      // http://stackoverflow.com/a/3485654/697856
-      document.body.style.backgroundColor = "rgba(0,0,0,0)";
-      //document.body.style.display='none';
-      document.body.offsetHeight;
-      //document.body.style.display='';
-    //}, 0);
-
     var isChecking = false;
-    document.head.appendChild(navDecorBlocker);
     var doNotTraceTags = [
       "svg", "p", "pre", "script", "style"
     ];
