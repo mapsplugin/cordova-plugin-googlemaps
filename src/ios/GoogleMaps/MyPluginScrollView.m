d1a35e8cab8e10e594daa7e1c8e4e8b56ffedb47
wf9a5m75 <wf9a5m75@gmail.com>
2018-03-09 16:29:42 -0800

    Fix: possible problem solution

--- src/ios/GoogleMaps/MyPluginScrollView.m
+++ src/ios/GoogleMaps/MyPluginScrollView.m
@@ -15,7 +15,13 @@
 {
   self = [super initWithFrame:aRect];
   self.debugView = [[MyPluginLayerDebugView alloc] initWithFrame:CGRectMake(0, 0, aRect.size.width, 5000)];
-
+  // Avoid the white bar that appears at the top of the map with iPhone iOS 11
+  // See problem description here: https://github.com/mapsplugin/cordova-plugin-googlemaps/issues/1909
+  //
+  // https://github.com/vidinoti/cordova-plugin-googlemaps/commit/0894072be260223f4ee833e422adf011af9740dd
+  if (@available(iOS 11, *)) {
+    self.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+  }
   return self;
 }
 
