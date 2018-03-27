73ec7f7a75b859fd831076ffa2be9798311bf2c8
wf9a5m75 <wf9a5m75@gmail.com>
2018-03-27 12:57:54 -0700

    Bug fix: xml2js error even in v2.2.9 #2182

--- src/before_plugin_rm.js
+++ src/before_plugin_rm.js
@@ -5,11 +5,20 @@ module.exports = function(ctx) {
       Q = ctx.requireCordovaModule('q');
   var projectRoot = ctx.opts.projectRoot,
     configXmlPath = path.join(projectRoot, 'config.xml');
-
   var versions = ctx.opts.cordova.version.split(/\./g);
+
+  var Module = require('module').Module;
   var NODE_MODULES_DIR = path.join(__dirname, '..', 'node_modules');
   if (fs.existsSync(NODE_MODULES_DIR)) {
-    module.paths.push(NODE_MODULES_DIR);
+    var old_nodeModulePaths = Module._nodeModulePaths;
+    var oldPaths = Module._nodeModulePaths(projectRoot);
+    if (oldPaths.indexOf(NODE_MODULES_DIR) === -1) {
+      Module._nodeModulePaths = function(from) {
+          var paths = old_nodeModulePaths.call(this, from);
+          paths.push(NODE_MODULES_DIR);
+          return paths;
+      };
+    }
   }
 
   var xml2js = require('xml2js');
