558b963045e16fcedd85c95a21303a5274b4b6a2
wf9a5m75 <wf9a5m75@gmail.com>
2018-04-03 17:11:26 -0700

    Fix: Application stuck in splash screen after install google maps native #2191

--- www/googlemaps-cdv-plugin.js
+++ www/googlemaps-cdv-plugin.js
@@ -8,95 +8,8 @@ if (!cordova) {
   });
 } else {
   var common = require('./Common');
-
   // The pluginInit.js must execute before loading HTML is completed.
   require("./pluginInit")();
-
-  (function() {
-    // If <body> is not ready yet, wait 25ms, then execute this function again.
-    if (!document.body || !document.body.firstChild) {
-      common.nextTick(arguments.callee, 25);
-      return;
-    }
-    var execCmd = require("./commandQueueExecutor");
-    var cordovaGoogleMaps = new (require('./CordovaGoogleMaps'))(execCmd);
-
-    // If the `transitionend` event is ocurred on the observed element,
-    // adjust the position and size of the map view
-    document.body.addEventListener("transitionend", function(e) {
-      if (!e.target.hasAttribute("__pluginDomId")) {
-        return;
-      }
-      cordovaGoogleMaps.invalidateN(5);
-    }, true);
-
-    // If the `scroll` event is ocurred on the observed element,
-    // adjust the position and size of the map view
-    var scrollEndTimer = null;
-    document.body.addEventListener("scroll", function(e) {
-      if (scrollEndTimer) {
-        clearTimeout(scrollEndTimer);
-      }
-      scrollEndTimer = setTimeout(function() {
-        common.nextTick(onScrollEnd);
-      }, 100);
-      cordovaGoogleMaps.followMapDivPositionOnly.call(cordovaGoogleMaps);
-    }, true);
-
-    function onScrollEnd() {
-      cordovaGoogleMaps.invalidate.call(cordovaGoogleMaps, {force: true});
-    }
-
-    // When the app is initialized,
-    // create the DOM hierarchy tree graph at once.
-    document.addEventListener("deviceready", onScrollEnd, {
-      once: true
-    });
-
-    // If the developer needs to recalculate the DOM tree graph,
-    // use `cordova.fireDocumentEvent('plugin_touch')`
-    document.addEventListener("plugin_touch", cordovaGoogleMaps.invalidate.bind(cordovaGoogleMaps));
-
-    // Repositioning 30 times when the device orientaion is changed.
-    window.addEventListener("orientationchange", function() {
-      cordovaGoogleMaps.invalidateN(50);
-    });
-
-
-    /*****************************************************************************
-     * Name space
-     *****************************************************************************/
-    module.exports = {
-      event: require('./event'),
-      Animation: {
-          BOUNCE: 'BOUNCE',
-          DROP: 'DROP'
-      },
-
-      BaseClass: require('./BaseClass'),
-      BaseArrayClass: require('./BaseArrayClass'),
-      Map: {
-        getMap: cordovaGoogleMaps.getMap.bind(cordovaGoogleMaps)
-      },
-      StreetView: {
-        getPanorama: cordovaGoogleMaps.getPanorama.bind(cordovaGoogleMaps)
-      },
-      HtmlInfoWindow: require('./HtmlInfoWindow'),
-      LatLng: require('./LatLng'),
-      LatLngBounds: require('./LatLngBounds'),
-      MapTypeId: require('./MapTypeId'),
-      environment: require('./Environment'),
-      Geocoder: require('./Geocoder')(execCmd),
-      LocationService: require('./LocationService')(execCmd),
-      geometry: {
-          encoding: require('./encoding'),
-          spherical: require('./spherical'),
-          poly: require('./poly')
-      }
-    };
-
-  }());
-
 
   cordova.addConstructor(function() {
       if (!window.Cordova) {
@@ -125,4 +38,88 @@ if (!cordova) {
         once: true
       });
   });
+
+  var execCmd = require("./commandQueueExecutor");
+  var cordovaGoogleMaps = new (require('./CordovaGoogleMaps'))(execCmd);
+
+  function onScrollEnd() {
+    cordovaGoogleMaps.invalidate.call(cordovaGoogleMaps, {force: true});
+  }
+
+  window.addEventListener("load", function() {
+    common.nextTick(function() {
+      // If the developer needs to recalculate the DOM tree graph,
+      // use `cordova.fireDocumentEvent('plugin_touch')`
+      document.addEventListener("plugin_touch", cordovaGoogleMaps.invalidate.bind(cordovaGoogleMaps));
+
+      // Repositioning 30 times when the device orientaion is changed.
+      window.addEventListener("orientationchange", function() {
+        cordovaGoogleMaps.invalidateN(30);
+      });
+
+      // If <body> is not ready yet, wait 25ms, then execute this function again.
+      // if (!document.body || !document.body.firstChild) {
+      //   common.nextTick(arguments.callee, 25);
+      //   return;
+      // }
+
+      // If the `transitionend` event is ocurred on the observed element,
+      // adjust the position and size of the map view
+      document.body.addEventListener("transitionend", function(e) {
+        if (!e.target.hasAttribute("__pluginDomId")) {
+          return;
+        }
+        cordovaGoogleMaps.invalidateN(5);
+      }, true);
+
+      // If the `scroll` event is ocurred on the observed element,
+      // adjust the position and size of the map view
+      var scrollEndTimer = null;
+      document.body.addEventListener("scroll", function(e) {
+        if (scrollEndTimer) {
+          clearTimeout(scrollEndTimer);
+        }
+        scrollEndTimer = setTimeout(function() {
+          common.nextTick(onScrollEnd);
+        }, 100);
+        cordovaGoogleMaps.followMapDivPositionOnly.call(cordovaGoogleMaps);
+      }, true);
+
+
+      onScrollEnd();
+    });
+  });
+
+  /*****************************************************************************
+   * Name space
+   *****************************************************************************/
+
+  module.exports = {
+    event: require('./event'),
+    Animation: {
+        BOUNCE: 'BOUNCE',
+        DROP: 'DROP'
+    },
+
+    BaseClass: require('./BaseClass'),
+    BaseArrayClass: require('./BaseArrayClass'),
+    Map: {
+      getMap: cordovaGoogleMaps.getMap.bind(cordovaGoogleMaps)
+    },
+    StreetView: {
+      getPanorama: cordovaGoogleMaps.getPanorama.bind(cordovaGoogleMaps)
+    },
+    HtmlInfoWindow: require('./HtmlInfoWindow'),
+    LatLng: require('./LatLng'),
+    LatLngBounds: require('./LatLngBounds'),
+    MapTypeId: require('./MapTypeId'),
+    environment: require('./Environment'),
+    Geocoder: require('./Geocoder')(execCmd),
+    LocationService: require('./LocationService')(execCmd),
+    geometry: {
+        encoding: require('./encoding'),
+        spherical: require('./spherical'),
+        poly: require('./poly')
+    }
+  };
 }
