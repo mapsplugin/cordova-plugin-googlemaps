f868957947210957086f84bca0b6144f49a51e7c
wf9a5m75 <wf9a5m75@gmail.com>
2018-06-03 15:19:49 -0700

    Fix: can not interact with map on tutorial code #2307

--- www/Map.js
+++ www/Map.js
@@ -211,9 +211,9 @@ Map.prototype.getMap = function(meta, div, options) {
         div = div.parentNode;
       }
     }
-    // cordova.fireDocumentEvent("plugin_touch", {
-    //   force: true
-    // });
+    cordova.fireDocumentEvent("plugin_touch", {
+      force: true
+    });
 
     //------------------------------------------------------------------------
     // In order to work map.getVisibleRegion() correctly, wait a little.
