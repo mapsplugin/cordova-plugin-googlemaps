0fac8c9bf0e48c714c8fe48c7abed65ab742cc07
wf9a5m75 <wf9a5m75@gmail.com>
2018-08-22 14:28:39 -0700

    (JS)Prevent JS error

--- www/HtmlInfoWindow.js
+++ www/HtmlInfoWindow.js
@@ -337,6 +337,7 @@ var HTMLInfoWindow = function () {
   };
 
   self._hook.on(self, "infoPosition_changed", function (ignore, point) {
+    if (!point) return;
     var x = point.x + self.get("offsetX");
     var y = point.y + self.get("offsetY");
     anchorDiv.style['-webkit-transform'] = "translate3d(" + x + "px, " + y + "px, 0px)";
