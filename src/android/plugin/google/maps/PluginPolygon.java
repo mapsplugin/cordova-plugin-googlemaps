c54b4c476d904d52b817388b829306db260ddaea
adappter <tarkan.goekdal@gmail.com>
2019-04-11 07:49:11 +0200

    Update PluginPolygon.java
    Removed unnecessary code setting the visibility of the polygon hardcoded to true/false.
    This is not needed, since the visibility is managed by the PolygonOptions.

--- src/android/plugin/google/maps/PluginPolygon.java
+++ src/android/plugin/google/maps/PluginPolygon.java
@@ -339,11 +339,6 @@ public class PluginPolygon extends MyPlugin implements MyPluginInterface  {
             public void run() {
                 // Update the polygon
                 polygon.setPoints(path);
-                if (path.size() > 0) {
-                    polygon.setVisible(true);
-                } else {
-                    polygon.setVisible(false);
-                }
                 callbackContext.success();
             }
         });
