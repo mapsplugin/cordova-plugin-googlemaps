f82f1db542d09ce6ed5d131d4a8fa9af98eb81c1
Masashi Katsumata <wf9a5m75@gmail.com>
2019-04-15 10:25:09 -0700

    Revert "Update PluginPolygon.java"

--- src/android/plugin/google/maps/PluginPolygon.java
+++ src/android/plugin/google/maps/PluginPolygon.java
@@ -340,7 +340,7 @@ public class PluginPolygon extends MyPlugin implements MyPluginInterface  {
                 // Update the polygon
                 polygon.setPoints(path);
                 if (path.size() > 0) {
-                    polygon.setVisible(polygon.isVisible());
+                    polygon.setVisible(true);
                 } else {
                     polygon.setVisible(false);
                 }
