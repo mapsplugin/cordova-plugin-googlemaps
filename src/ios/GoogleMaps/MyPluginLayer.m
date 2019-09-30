35cde530ffcb63a8311045adae2b1ffc6cf95489
wf9a5m75 <wf9a5m75@gmail.com>
2019-09-30 12:58:43 -0700

    fix: can't click buttons on the Cordova wheel dialog #2676

--- src/ios/GoogleMaps/MyPluginLayer.m
+++ src/ios/GoogleMaps/MyPluginLayer.m
@@ -347,7 +347,7 @@
   NSArray *subviews = [self.webView.superview subviews];
   //CGRect statusBarFrame = [UIApplication sharedApplication].statusBarFrame;
   //CGPoint subviewPoint = CGPointMake(browserClickPoint.x, browserClickPoint.y - statusBarFrame.size.height);
-  CGPoint subviewPoint = CGPointMake(browserClickPoint.x, browserClickPoint.y + self.webView.frame.origin.y);
+
   for (int i = ((int)[subviews count] - 1); i >= 0; i--) {
     subview = [subviews objectAtIndex: i];
     //NSLog(@"--->subview[%d] = %@", i, subview);
@@ -360,6 +360,7 @@
       continue;
     }
 
+    CGPoint subviewPoint = CGPointMake(point.x, point.y - subview.frame.origin.y);
     UIView *hit = [subview hitTest:subviewPoint withEvent:event];
 
     if (hit) {
