f722b9ec8023cd2a28a9b5f73b414f970e42e565
wf9a5m75 <wf9a5m75@gmail.com>
2020-05-22 19:13:45 -0700

    Remove tile aggressively.

--- src/browser/PluginTileOverlay.js
+++ src/browser/PluginTileOverlay.js
@@ -77,7 +77,13 @@ function TileOverlay(mapId, hashCode, options) {
 
     releaseTile: function(div) {
       var cacheId = div.getAttribute('cacheId');
-      delete tileCaches[cacheId];
+      var div = tileCaches[cacheId];
+      if (div) {
+        if (div.parentNode) {
+          div.parentNode.removeChild(div);
+        }
+        delete tileCaches[cacheId];
+      }
     },
 
     remove: function() {
