4c4c961c99f3b58c5ffcfedf43d005b8765033aa
Itsaso Aguirre Lorenzo <itsaso.aguirre@plainconcepts.com>
2024-02-02 13:30:37 +0100

    (ios) fix build errors caused with cordova-ios@7 due to CDVCommandDelegateImpl

--- src/ios/GoogleMaps/PluginMap.m
+++ src/ios/GoogleMaps/PluginMap.m
@@ -667,7 +667,7 @@
     }
 
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    [(CDVCommandDelegateImpl *)self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
   }];
 }
 
